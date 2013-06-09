3f90aa0676586d276cb55168e761e9333f58d994
Noumbissi <xnoumbis@uwaterloo.ca>
2013-06-09 11:40:01 -0400

    Some new stuff
    	modified:   proposal/ctaint-proposal.bib
    	modified:   proposal/ctaint-proposal.tex
    	new file:   resources/livshits-java-taint05.pdf
    	modified:   src/ctaint.cpp
    	modified:   src/ctaint.hpp

--- src/ctaint.cpp
+++ src/ctaint.cpp
@@ -1,1 +1,12 @@
 #include "ctaint.hpp"
+
+char CTaintFunctionPass::ID = 0;
+
+bool CTaintFunctionPass::runOnFunction(Function &F) {
+  errs() << "[CTaint]: ";
+  errs().write_escaped(F.getName()) << "\n";
+  return false;
+}
+
+static RegisterPass<CTaintFunctionPass>
+X("hello", "Hello World Pass", false, false);
--- src/ctaint.hpp
+++ src/ctaint.hpp
@@ -1,6 +1,23 @@
 #ifndef CTAINT_HPP
 #define CTAINT_HPP
 
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Support/raw_ostream.h"
 
+using namespace llvm;
+
+namespace {
+
+struct CTaintFunctionPass : public FunctionPass {
+
+  static char ID;
+
+  CTaintFunctionPass() : FunctionPass(ID) {}
+
+  virtual bool runOnFunction(Function &F);
+
+};
+}
 
 #endif //CTAINT_HPP
