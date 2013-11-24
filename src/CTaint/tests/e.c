6649539b6ad12a16c89361b4082742036e1458d8
Noumbissi <xnoumbis@uwaterloo.ca>
2013-11-23 19:14:42 -0500

    Adding new files too use for preliminary tests
    	new file:   tests/e.c
    	new file:   tests/mongoose.bc
    	new file:   tests/mongoose.c
    	new file:   tests/mongoose.h
    	new file:   tests/mongoose.opt.bc
    	new file:   tests/mongoose.opt.ll
    	renamed:    t.c -> tests/t.c
    	new file:   tests/test.bc

---
+++ src/CTaint/tests/e.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    char buf[10];
+    char* c = buf;
+    char *b = "q";
+    char *f = b;
+    scanf ("%s", c);
+    return 0;
+}
