551ae9b06b61582f1dc293d6b7cb2eeceb4c902c
Noumbissi <xnoumbis@uwaterloo.ca>
2014-06-18 19:55:17 -0400

    Removed non used files

--- final-exam/q4/runZ3.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-PARAMS="$@"
-
-wine z3-4.3.0-x86/bin/z3.exe "$PARAMS"
--- src/configure-poolalloc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./configure --prefix=$LLVM_HOME/build/Release+Asserts \
-  --with-llvmsrc=$LLVM_HOME \
-  --with-llvmobj=$LLVM_HOME/build
